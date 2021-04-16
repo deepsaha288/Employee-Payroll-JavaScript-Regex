6f33098569c7b29c7fb15d0cbfe1cd2c68f1154a
Deep Saha <deepsaha288@yahoo.com>
2021-04-16 16:04:50 +0530

    Extending the Class

--- employepayroll.js
+++ employepayroll.js
@@ -1,18 +1,23 @@
 class EmployeePayroll{
     id;
     salary;
+    gender
+    startdate;
 
-    constructor(id,name,salary){
-        this.id=id;
-        this.name=name;
-        this.salary=salary;
+    constructor(...params){
+        this.id=params[0];
+        this.name=params[1];
+        this.salary=params[2];
+        this.gender=params[3];
+        this.startdate=params[4];
     } 
 
     get name(){ return this._name;}
     set name(name){ this._name=name;}
 
     toString(){
-        return "id=" +this.id+ ", name= " +this.name+ ", salary= " +this.salary;
+        return "id=" +this.id+ ", name= " +this.name+ ", salary= " +this.salary+ " , gender= " +this.gender+
+        " , startdate= "+this.startdate;
     }
 }
 
@@ -21,3 +26,6 @@ console.log(employeePayroll.toString());
 
 employeePayroll.name="Manish";
 console.log(employeePayroll.toString());
+
+let newemployeePayroll = new EmployeePayroll(2,"Roshni",98000,"F",new Date("03-02-2019"));
+console.log(newemployeePayroll.toString());
