f643c53a9192606b61eed56182d9c9b3c472d39c
Deep Saha <deepsaha288@yahoo.com>
2021-04-16 17:04:49 +0530

    checking Gender validation

--- employepayroll.js
+++ employepayroll.js
@@ -5,10 +5,23 @@ class EmployeePayroll{
     startdate;
 
     constructor(...params){
-        this.id=params[0];
+        let nonzeroregex=RegExp('^[0-9]*[1-9][0-9]*$');
+        let genderregex=RegExp('[M]{1}||[F]{1}');
+        
+        if(nonzeroregex.test(params[0])){
+            this.id=params[0];
+        }else{throw "Incorrect Id";}
+        
         this.name=params[1];
-        this.salary=params[2];
-        this.gender=params[3];
+        
+        if(nonzeroregex.test(params[2])){
+            this.salary=params[2];
+        }else{throw "Incorrect Salary";}
+        
+        if(genderregex.test(params[3])){
+            this.gender=params[3];
+        }else{throw "Incorrect Gender";}
+
         this.startdate=params[4];
     } 
 
@@ -32,11 +45,22 @@ class EmployeePayroll{
 let employeePayroll = new EmployeePayroll(1,"Deep",20000);
 console.log(employeePayroll.toString());
 try{
-    employeePayroll.name="manish";
+    employeePayroll.name="Manish";
     console.log(employeePayroll.toString());
 }catch(e){
     console.error(e);
 }
 
-let newemployeePayroll = new EmployeePayroll(2,"Roshni",98000,"F",new Date("03-02-2019"));
-console.log(newemployeePayroll.toString());
+try{
+    let newemployeePayroll = new EmployeePayroll(2,"Roshni",98000,"Female",new Date("03-02-2019"));
+    console.log(newemployeePayroll.toString());
+}catch(e){
+    console.error(e);
+}
+
+try{
+    let employeePayroll3 = new EmployeePayroll(3,"Anup",99999,"M",new Date("04-21-2018"));
+    console.log(employeePayroll3.toString());
+}catch(e){
+    console.error(e);
+}
